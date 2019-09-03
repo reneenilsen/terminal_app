d505b0cb36bf1bb2a04cf3e3517130149e6a3081
Renee Nilsen <reneen3833@gmail.com>
2019-09-03 14:08:46 +1000

    pokemon class spawn rate

--- pokemon_class.rb
+++ pokemon_class.rb
@@ -1,19 +1,32 @@
 class Pokemon
-attr_accessor :pokemon_type, :pokemon_level, :spawn, :current_pokemon_level, :current_pokemon_type
+attr_accessor :pokemon_type, :pokemon_level, :spawn, :current_pokemon_level, :current_pokemon_type, :spawn_rate
 
     def initialize
         @pokemon_type = ["Pikachu", "Eevee", "Psyduck", "Ekans", "Vulpix", "Squirtle", "Bulbasaur", "Charmander"]
         @pokemon_level = [1, 2, 3, 4, 5]
+       
     end
 
-    def spawn
-        @current_pokemon_type = @pokemon_type.sample(1)
-        @current_pokemon_level = @pokemon_level.sample(1)
-        puts "You found a #{@current_pokemon_type}"
-        puts "Its level is #{@current_pokemon_level}"
+    def spawn_rate
+        @spawn_rate = rand(1..100)
+        puts "Spawn rate number is #{@spawn_rate}" 
+            if @spawn_rate < 30
+                puts "You found a pokemon"
+                @current_pokemon_type = @pokemon_type.sample(1)
+                @current_pokemon_level = @pokemon_level.sample(1)
+                puts "You found a #{@current_pokemon_type}"
+                puts "Its level is #{@current_pokemon_level}"
+            elsif
+                p "There is no Pokemon here"
+            end  
     end
 
-    
+    # def spawn
+    #     @current_pokemon_type = @pokemon_type.sample(1)
+    #     @current_pokemon_level = @pokemon_level.sample(1)
+    #     puts "You found a #{@current_pokemon_type}"
+    #     puts "Its level is #{@current_pokemon_level}"
+    # end
 end
 
 # eevee = Pokemon.new("Eevee", 3)
